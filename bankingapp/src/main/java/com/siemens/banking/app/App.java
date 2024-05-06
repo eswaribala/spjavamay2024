4c734dff23059039e505fc242b91a49d73e9ee5b
Parameswari Bala <parameswaribala@gmail.com>
2024-05-06 14:24:52 +0530

    updated

---
+++ bankingapp/src/main/java/com/siemens/banking/app/App.java
@@ -0,0 +1,10 @@
+package com.siemens.banking.app;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           System.out.println("Rocking with Java.....");
+	}
+
+}
