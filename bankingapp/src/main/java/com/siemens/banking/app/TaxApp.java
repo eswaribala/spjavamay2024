1c9d447e3ece28fe5decfc50c4dc411bdf61a1ea
Parameswari Bala <parameswaribala@gmail.com>
2024-05-07 09:57:24 +0530

    updated

--- bankingapp/src/main/java/com/siemens/banking/app/TaxApp.java
+++ bankingapp/src/main/java/com/siemens/banking/app/TaxApp.java
@@ -9,7 +9,7 @@ public class TaxApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int salary=0;
-		Gender gender=Gender.MALE;
+		Gender gender=null;
 		Scanner scanner=new Scanner(System.in);
 		System.out.println("Enter Your Salary");
 		salary=scanner.nextInt();
@@ -20,8 +20,57 @@ public class TaxApp {
 		//string to enum
 		gender=Gender.valueOf(scanner.nextLine());
 		System.out.println(gender);
-		
+		System.out.println(computeTax(gender,salary));
 
+	}
+	
+	
+	public static String computeTax(Gender gender, int salary) {
+		double tax=0.0;
+		String message=null;
+		if(gender.equals(Gender.MALE)){
+			
+			if((salary>0)&&(salary<=180000)) {
+				message= "No Tax";
+			}
+			else if((salary>=180001)&&(salary<=500000)){
+				
+				tax=salary*.10;
+				message="Computed Tax="+tax;
+			}
+			else if((salary>=500001)&&(salary<=800000)){
+				
+				tax=salary*.20;
+				message="Computed Tax="+tax;
+			}else {
+				tax=salary*.30;
+				message="Computed Tax="+tax;
+			}
+			
+			return message;
+		}else
+		{
+			if((salary>0)&&(salary<=190000)) {
+				message= "No Tax";
+			}
+			else if((salary>=190001)&&(salary<=500000)){
+				
+				tax=salary*.10;
+				message="Computed Tax="+tax;
+			}
+			else if((salary>=500001)&&(salary<=800000)){
+				
+				tax=salary*.20;
+				message="Computed Tax="+tax;
+			}else {
+				tax=salary*.30;
+				message="Computed Tax="+tax;
+			}
+			
+			return message;
+		}
+		
+		
 	}
 
 }
