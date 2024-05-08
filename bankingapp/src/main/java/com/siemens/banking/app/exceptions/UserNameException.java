5017747bf857f32fe4fb53bbf92324939b4c3cc5
Parameswari Bala <parameswaribala@gmail.com>
2024-05-08 15:49:41 +0530

    updated

---
+++ bankingapp/src/main/java/com/siemens/banking/app/exceptions/UserNameException.java
@@ -0,0 +1,12 @@
+package com.siemens.banking.app.exceptions;
+
+public class UserNameException extends RuntimeException{
+
+	public UserNameException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+}
